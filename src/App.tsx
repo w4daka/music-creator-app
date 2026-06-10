c17cd9352384339666c2c249cde6ad454f03a58e
w4daka <213398204+w4daka@users.noreply.github.com>
2026-06-10 16:04:56 +0900

    feat生成するボタンを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">音楽生成アプリ</h1>
+      <Button>生成する</Button>
     </div>
   );
 }
